58906cc00430655e098ef0837a152ac2e62d7d1e
halitklyc <halitenes.kalayci@gmail.com>
2025-06-13 19:25:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2 #opencv-python wrapper
+import matplotlib.pyplot as plt
+
+
+# RGB
+# BGR
+
+# OpenCV imageları BGR olarak okur, ama matplotlib ve çoğu yeni kütüphane RGB ile çalışır..
+img = cv2.imread('kedi.jpg')
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+print(type(img_rgb))
+print(img_rgb)
+print(img_rgb.ndim)
+print(img_rgb.shape)
+
+plt.imshow(img_gray, cmap="gray")
+plt.axis('off')
+plt.show()
