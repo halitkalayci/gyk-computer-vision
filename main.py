c46271f29d32f067b7741a98ceb3bca5c01e4467
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-06-16 19:24:49 +0300

    Görüntü işleme işlemleri eklendi: yeniden boyutlandırma, döndürme, yansıtma, histogram eşitleme, bulanıklık ve eşikleme. Sonuç görüntüsü gösterildi.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2 #opencv-python wrapper
-import matplotlib.pyplot as plt
 
 
 # RGB
@@ -8,15 +7,34 @@ import matplotlib.pyplot as plt
 # OpenCV imageları BGR olarak okur, ama matplotlib ve çoğu yeni kütüphane RGB ile çalışır..
 img = cv2.imread('kedi.jpg')
 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+height, width = img.shape[:2]
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Resize
+resized_img = cv2.resize(img, (width // 2, height // 2)) # (100,100) => width, height
 
-print(type(img_gray))
-print(img_gray)
-print(img_gray.ndim)
-print(img_gray.shape)
+# Rotate (Döndürme)
+center = (width // 2, height // 2) ## o anki yükseklik ve genişlik ortası.
+rotation_matrix = cv2.getRotationMatrix2D(center, 90, 1.0)
+rotated_img = cv2.warpAffine(img, rotation_matrix, (width, height))
 
-plt.imshow(img_gray, cmap="gray")
-plt.axis('off')
-plt.show()
+# Flip (Yansıma)
+flipped_img = cv2.flip(img, -1) # 1=> Yatay yansıma, 0=>Dikey Yansıma, -1 => Hem yatay hem de dikey yansıma
+
+# Histogram Eşitleme - (Kontrastı artırır) Görüntülerdeki netliği artırmak. - Grayscale için kullanılır.
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+equalized_img = cv2.equalizeHist(grayscale)
+
+# Blur (Bulanıklık) => Ek araştırma (Opsiyonel)
+blurred_img = cv2.GaussianBlur(img, (15,15), 0)
+
+# Thresholding (Eşikleme) =>  Nesne-Arka plan ayrımı için kullanılır.
+# Gürültü Temizleme
+_, thresholded_img = cv2.threshold(grayscale, 127, 255, cv2.THRESH_BINARY) # 127 => Eşik değeri, 255 => Maksimum değer, cv2.THRESH_BINARY => Binary eşikleme
+
+
+
+
+#Boilerplate Kod => Basmakalıp kod
+cv2.imshow('Kedi', thresholded_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows() # Bir tuşa basılana kadar imageı göster.
