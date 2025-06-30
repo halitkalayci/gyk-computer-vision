43f484dd306c545bda8dd4e231f1d31694754365
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-06-30 20:12:56 +0300

    YOLO modelini eğitmek için yeni bir 'train' fonksiyonu eklendi. Ana program akışı düzenlendi ve eğitim sonrası inceleme notları eklendi.

--- plate-project/main.py
+++ plate-project/main.py
@@ -5,4 +5,21 @@
 
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+
+def train():
+    model = YOLO("yolov8n.pt")
+    model.train(data="data.yaml", epochs=10, imgsz=640, batch=8)
+    #1955/8
+
+# Pythondaki main method.
+# python main.py => Çalış
+# başka bir dosyadan import edildiğinde çalışmaz.
+if __name__ == "__main__":
+    train()
+
+
+# Eğitimi bitirelim.
+# Oluşan klasörü inceleyelim. 
+# İçerisindeki "best.pt" dosyasını kullanacağız.
+# Bu pt dosyası ile birlikte tahmin yapacak yeni bir dosyada bir GUI uygulama yapalım.
+# 20:30
