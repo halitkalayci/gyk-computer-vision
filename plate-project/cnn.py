97dc55ae2fb2a45074e586b96961584ae920d1b9
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-07-02 20:30:06 +0300

    latest

--- plate-project/cnn.py
+++ plate-project/cnn.py
@@ -3,3 +3,24 @@
 # imageları process
 # label dosyalarını oku.
 # imagelardan ve labellardan veri seti oluştur.
+
+import numpy as np
+
+X = np.load("X.npy")
+y = np.load("y.npy")
+
+print(X[1])
+print(y.shape)
+
+# X,y train_test_split
+# Tensorflow sequential modeli oluştur.
+# modeli compile et.
+
+# Activation fonksiyonları nelerdir, ne için kullanılır?
+# Loss function nedir, ne için kullanılır?
+# Optimizer nedir, ne için kullanılır?
+
+# Yazı hazırlamak.
+
+# Sonraki derse tam kurulumlarla gelmek. Python 3.8-3.11 
+#Tensorflow kurulu.
