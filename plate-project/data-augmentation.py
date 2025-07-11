8fc53a326443800a72ff78bf0c85155ec4722343
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-07-11 20:10:14 +0300

    Veri artırma işlemleri için sınıf etiketleri ve sınırlayıcı kutular eklendi. Görsellerin kaydedilmesi ve etiket dosyalarının oluşturulması sağlandı. Ayrıca, zoom oranı %20'den %40'a çıkarıldı.

--- plate-project/data-augmentation.py
+++ plate-project/data-augmentation.py
@@ -1,13 +1,22 @@
 import cv2
 from matplotlib import pyplot as plt
 import albumentations as A
-
+import os 
 image_path = "data/images/1.jpg"
 
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+bboxes = []
+class_labels = []
 
+with open("data/labels/1.txt", "r") as f:
+    for line in f:
+        parts = line.strip().split()
+        class_id = int(parts[0])
+        x_center, y_center, width, height = map(float, parts[1:])
+        bboxes.append([x_center, y_center, width, height])
+        class_labels.append(class_id)
 
 augmentations = [
     # Görseli rastgele -20 ile +20 arasında döndürür.
@@ -20,7 +29,7 @@ augmentations = [
     ("Brightness", A.RandomBrightnessContrast(p=1.0)),
 
     # Görseli rastgele %20 oranına kadar büyütür veya küçültür.
-    ("Zoom", A.RandomScale(scale_limit=0.2, p=1.0)),
+    ("Zoom", A.RandomScale(scale_limit=0.4, p=1.0)),
 
     # Probability => 1.0
     # Focus problemi olan fotoğraf gibi..
@@ -42,13 +51,61 @@ augmentations = [
 
 plt.figure(figsize=(18,12))
 for i, (title,aug) in enumerate(augmentations,start=2):
-    augmented_image = aug(image=image)["image"]
+    image_aug = cv2.imread(image_path)
+    image_aug = cv2.cvtColor(image_aug, cv2.COLOR_BGR2RGB)
+
+    transform = A.Compose(
+        [aug],
+        bbox_params=A.BboxParams(format='yolo', label_fields=["class_labels"])
+    )
+
+    #try:
+    augmented = transform(image=image_aug, bboxes=bboxes, class_labels=class_labels)
+    image_aug = augmented["image"]
+    bboxes = augmented["bboxes"]
+    labels = augmented["class_labels"]
+
+    # 1.jpg -> Horizontal Flip
+    # 1_Horizontal_Flip.jpg
+    
+    base_img_name = os.path.splitext(os.path.basename(image_path))[0]
+    print( "BaseImgName:", base_img_name)
+
+    img_name = f"data/images/{base_img_name}_{title.replace(' ', '_')}.jpg"
+    label_name = f"data/labels/{base_img_name}_{title.replace(' ', '_')}.txt"
+
+
+    cv2.imwrite(img_name, cv2.cvtColor(image_aug, cv2.COLOR_RGB2BGR))
+
+    with open(label_name, "w") as f:
+        for bbox in bboxes:
+            print(f"0 {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}\n")
+            f.write(f"0 {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}\n")
+
+    h, w, _ = image_aug.shape
+
+    for bbox in bboxes:
+        print(bbox)
+        x_center, y_center, width, height = bbox
+        x1 = int((x_center - width/2) * w)
+        x2 = int((x_center + width/2) * w)
+        y1 = int((y_center - height/2) * h)
+        y2 = int((y_center + height/2) * h)
+        cv2.rectangle(image_aug, (x1,y1), (x2,y2), (255,0,0), 5)
+
     plt.subplot(3,3,i)
-    plt.imshow(augmented_image)
+    plt.imshow(image_aug)
     plt.title(title)
     plt.axis("off")
+
+    #except Exception as e:
+       #print(f"Error applying {title}: {e}")
+       # continue
 
 plt.tight_layout()
 plt.show()
 
 # Augmente görselleri kaydet.
+
+# 1.jpg -> 1_blur.jpg 1_zoom.jpg 1_rotate.jpg 1_horizontal_flip.jpg 1_brightness.jpg 1_gauss_noise.jpg 1_cutout.jpg 1_motion_blur.jpg 1_median_blur.jpg
+# 1.txt -> 1_blur.txt 1_zoom.txt 1_rotate.txt 1_horizontal_flip.txt 1_brightness.txt 1_gauss_noise.txt 1_cutout.txt 1_motion_blur.txt 1_median_blur.txt
